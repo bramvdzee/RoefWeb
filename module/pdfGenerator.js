ff9283c05d40607f54602eefaeae5ab055519eea
bramvdzee <bramvdzee1@gmail.com>
2017-03-27 21:43:59 +0200

    >

--- module/pdfGenerator.js
+++ module/pdfGenerator.js
@@ -8,64 +8,10 @@ module.exports = {
 
         var fileName = "Dagstaat_" + id + ".pdf";
 
-        var pageSize = "A4",
-        pageOrientation = "Landscape",
-        dpi = 300, //from experimenting with different combinations of viewportSize and paperSize the pixels per inch comes out to be 150
-        pdfViewportWidth = 3508,
-        pdfViewportHeight = 2480,
-        cmToInchFactor = 0.393701,
-        widthInInches,
-        heightInInches,
-        temp;
-
-    switch(pageSize){
-        case 'Letter':
-        default:
-            widthInInches = 8.5;
-            heightInInches = 11;
-            break;
-        case 'Legal':
-            widthInInches = 8.5;
-            heightInInches = 14;
-            break;
-        case 'A3':
-            widthInInches = 11.69
-            heightInInches = 16.54;
-            break;
-        case 'A4':
-            widthInInches = 8.27;
-            heightInInches = 11.69;
-            break;
-        case 'A5':
-            widthInInches = 5.83;
-            heightInInches = 8.27;
-            break;
-        case 'Tabloid':
-            widthInInches = 11;
-            heightInInches = 17;
-            break;
-    }
-
-    //reduce by the margin (assuming 1cm margin on each side)
-    widthInInches-= 2*cmToInchFactor;
-    heightInInches-= 2*cmToInchFactor;
-
-    //interchange if width is equal to height
-    if(pageOrientation === 'Landscape'){
-        temp = widthInInches;
-        widthInInches = heightInInches;
-        heightInInches = temp;
-    }
-
-    //calculate corresponding viewport dimension in pixels
-    pdfViewportWidth = dpi*widthInInches;
-    pdfViewportHeight = dpi*heightInInches;
-
-
         phantom.create().then(function(ph) {
             ph.createPage().then(function(page) {
-                page.property('viewportSize', { width: pdfViewportWidth, height: pdfViewportHeight }  );
-                page.property('paperSize',{  format: pageSize,  orientation: pageOrientation, margin: '0cm' });
+                page.property('viewportSize', { width: 3508, height: 2480 }  );
+                page.property('paperSize',{  format: 'a4', dpi: 300,  orientation: 'landscape', margin: '0cm' });
                 page.open(req.protocol + "://" + req.get("host") + "/dagstaat/" + id + "/pdf").then(function(status) {
                     page.render(fileName).then(function() {
                         res.download(fileName, fileName, function(err){
